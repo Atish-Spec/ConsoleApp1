a80d67d0c64cadcdfacbb25e44768384a37f03be
Atish-Spec <atishhanvate2014@GMAIL.COM>
2024-08-27 14:45:50 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.Design;
+
+namespace ConsoleApp1
+{
+    
+    
+    public class Program
+    {
+        static void Main(string[] args)       
+        {
+
+
+            string abc = "Abc";
+
+                    
+        }
+    }
+}
